d9dc30b00b45457bdd5106a5758ec7bb67313d28
kitou88C39 <kanfutrooper@icloud.com>
2022-12-17 16:40:26 +0900

    Links.jsを作成及び編集

---
+++ components/Footer.js
@@ -0,0 +1,25 @@
+import styles from '../styles/Home.module.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <a
+        href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+        className={styles.card}
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <h2 className={inter.className}>
+          Deploy <span>-&gt;</span>
+        </h2>
+        <p className={inter.className}>
+          Instantly deploy your Next.js site to a shareable URL
+          with&nbsp;Vercel.
+        </p>
+      </a>
+    </footer>
+  );
+}
