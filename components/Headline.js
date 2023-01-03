34aa976ffd9b3a829963ff10e6f646f6d580d571
kitou88C39 <kanfutrooper@icloud.com>
2023-01-03 11:47:14 +0900

    headline.js 作成

---
+++ components/Headline.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css';
+
+export default function Headline() {
+  return (
+    <div>
+      <h1 className={styles.title}>Index Page</h1>
+
+      <p className={styles.description}>
+        Get started by editing
+        <code className={styles.code}>pages/index.js</code>
+      </p>
+    </div>
+  );
+}
