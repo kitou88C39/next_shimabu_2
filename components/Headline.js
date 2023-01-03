e32681cc40107c32f62ebc2693e9ade366b4caf7
kitou88C39 <kanfutrooper@icloud.com>
2023-01-03 15:27:53 +0900

    headline.js 編集

--- components/Headline.js
+++ components/Headline.js
@@ -9,7 +9,7 @@ export default function Headline(props) {
 
       <p className={styles.description}>
         Get started by editing
-        <code className={styles.code}>pages/{props.page}.js</code>
+        <code className={styles.code}>Pages/{props.page}.js</code>
       </p>
     </div>
   );
