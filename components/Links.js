edd49e67c035cfbb3acb4e7c966847cfb94dc271
kitou88C39 <kanfutrooper@icloud.com>
2023-01-13 22:06:48 +0900

    Links.jsのリファクタリング　

--- components/Links.js
+++ components/Links.js
@@ -5,25 +5,25 @@ const inter = Inter({ subsets: ['latin'] });
 const ITEMS = [
   {
     href: 'https://nextjs.org/docs',
-    title: 'Documentation & rarr;',
+    title: 'Documentation  ➡ ',
     description:
       'Find in-depth information about Next.js features and&nbsp;API.',
   },
   {
     href: 'https://nextjs.org/learn',
-    title: 'Learn & rarr;',
+    title: 'Learn  ➡︎ ',
     description:
       'Learn about Next.js in an interactive course with&nbsp;quizzes!',
   },
   {
     href: 'https://nextjs.org/templates ',
-    title: 'Templates & rarr;',
+    title: 'Templates  ➡︎ ',
     description:
       'Discover and deploy boilerplate example Next.js&nbsp;projects.',
   },
   {
     href: 'https://nextjs.org/deploy',
-    title: 'Deploy & rarr;',
+    title: 'Deploy  ➡︎ ',
     description:
       'Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.',
   },
@@ -35,7 +35,7 @@ export function Links() {
       <div className={styles.grid}>
         {ITEMS.map((item) => {
           return (
-            <a href={item.href} className={styles.card}>
+            <a key={item.href} href={item.href} className={styles.card}>
               <h2 className={styles.title}>{item.title}</h2>
               <p className={styles.description}>{item.description}</p>
             </a>
