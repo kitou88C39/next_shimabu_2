dff4afc6fa42455578c0a5bcda0200f8f5047f82
kitou88C39 <kanfutrooper@icloud.com>
2023-01-14 16:55:31 +0900

    ファイルのリファクタリング

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import styles from '../styles/Home.module.css';
-import { Footer } from '../components/Footer';
-import { Main } from '../components/Main';
-import { Header } from '../components/Header';
+import { Footer } from '../components/Footer/index';
+import { Main } from '../components/Main/index';
+import { Header } from '../components/Header/index';
 
 const inter = Inter({ subsets: ['latin'] });
 
