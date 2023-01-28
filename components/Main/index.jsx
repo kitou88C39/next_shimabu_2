5288ac83a130ce834022ec2a696cf1fb958d5141
kitou88C39 <kanfutrooper@icloud.com>
2023-01-28 19:55:27 +0900

    Main.jsx 編集

--- components/Main/index.jsx
+++ components/Main/index.jsx
@@ -6,6 +6,12 @@ import { Headline } from '../Headline';
 const inter = Inter({ subsets: ['latin'] });
 
 export function Main(props) {
+  useEffect(() => {
+    document.body.style.backgroundColor = 'lightblue';
+    return () => {
+      document.body.style.backgroundColor = '';
+    };
+  }, []);
   return (
     <main className={styles.main}>
       <Headline page={props.page}>
