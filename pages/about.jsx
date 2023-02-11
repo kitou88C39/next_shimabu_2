280de900d7cf1c4c4ae12723827805fa847b3d52
kitou88C39 <kanfutrooper@icloud.com>
2023-02-11 21:37:34 +0900

    useInputArray.jsxを作成及び編集

--- pages/about.jsx
+++ pages/about.jsx
@@ -9,21 +9,6 @@ import { useCounter } from 'hooks/useCounter';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// const useCounter = () => {
-//   const [count, setCount] = useState(1);
-//   const [isShow, setIsShow] = useState(true);
-//   const handleClick = useCallback(() => {
-//     if (count < 10) {
-//       setCount((prevCount) => prevCount + 1);
-//     }
-//   }, [count]);
-//   const handleDisplay = useCallback(() => {
-//     setIsShow((prevIsShow) => !prevIsShow);
-//   }, []);
-
-//   return { count, isShow, handleClick, handleDisplay };
-// };
-
 export default function Home() {
   const { count, isShow, handleClick, handleDisplay } = useCounter();
   const [text, setText] = useState('');
