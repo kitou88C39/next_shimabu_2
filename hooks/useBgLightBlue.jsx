808bcf1f3385a991adcadecea6f0629ceb38df65
kitou88C39 <kanfutrooper@icloud.com>
2023-02-13 16:52:11 +0900

    useBgLightBlue.jsxをuseRouterで編集

--- hooks/useBgLightBlue.jsx
+++ hooks/useBgLightBlue.jsx
@@ -1,10 +1,14 @@
 import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export const useBgLightBlue = () => {
+  const router = useRouter();
+
   useEffect(() => {
-    document.body.style.backgroundColor = 'lightblue';
+    document.body.style.backgroundColor =
+      router.pathname === '/' ? 'lightblue' : 'pink';
     return () => {
       document.body.style.backgroundColor = '';
     };
-  }, []);
+  }, [router.pathname]);
 };
